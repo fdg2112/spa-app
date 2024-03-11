7c624f210361c9b19570fbffde68ef48e3850375
fdg2112 <fdg2112@gmail.com>
2024-03-11 18:28:57 -0300

    secciones andando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { HomeComponent } from "./home/home.component";
+import { FooterComponent } from "./footer/footer.component";
+import { ContactComponent } from "./contact/contact.component";
+import { AboutComponent } from './about/about.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HeaderComponent, HomeComponent, FooterComponent, ContactComponent, AboutComponent]
 })
 export class AppComponent {
   title = 'spa-app';
